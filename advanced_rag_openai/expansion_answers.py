1a50637166e3de60883fdbfb5d808a87184afaa3
Marco Parisi <marco.parisi@synesthesia.it>
2024-08-17 09:40:50 +0200

    wip graph

--- advanced_rag_openai/expansion_answers.py
+++ advanced_rag_openai/expansion_answers.py
@@ -1,4 +1,4 @@
-from helper_utils import word_wrap
+from helper_utils import word_wrap, project_embeddings
 import os
 from pypdf import PdfReader
 from openai import OpenAI
@@ -9,6 +9,7 @@ from langchain.text_splitter import (
 )
 from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
 from chromadb import PersistentClient
+import umap
 
 DIRECTORY_PATH = os.path.join(os.path.dirname(__file__), "documents")
 DATABASE_PATH = os.path.join(os.path.dirname(__file__), "database")
@@ -56,9 +57,51 @@ chroma_collection.add(ids=ids, documents=token_split_texts)
 chroma_collection.count()
 
 
-query = "What are steps to learn how to take notes?"
+query = "How to start taking notes?"
 
 results = chroma_collection.query(query_texts=[query], n_results=5)
 retrieved_documents = results["documents"][0]
 
-print("Retrieved documents: ", retrieved_documents)
+# Initialize OpenAI client
+openai_client = OpenAI(api_key=openai_api_key)
+
+def augment_query_generated(query, model="gpt-3.5-turbo"):
+    prompt = """You are a student who is trying to learn how to take notes. 
+    You have read a book on the topic and want to use those information to answer any question to a friend."""
+    messages = [{"role": "system", "content": prompt}, {"role": "user", "content": query}]
+
+    reponse = openai_client.chat.completions.create(
+        model=model,
+        messages=messages, )
+    content = reponse.choices[0].message["content"]
+    return content
+
+original_query = "What are the steps needed to start taking good notes?"
+hypothetical_answer = augment_query_generated(original_query)
+
+joint_query = f"{original_query} {hypothetical_answer}"
+print(word_wrap(joint_query))
+
+results = chroma_collection.query(
+    query_texts=joint_query, n_results=5, include=["documents", "embeddings"]
+)
+retrieved_documents = results["documents"][0]
+
+embeddings = chroma_collection.get(include=["embeddings"])["embeddings"]
+umap_transform = umap.UMAP(random_state=0, transform_seed=0).fit(embeddings)
+projected_dataset_embeddings = project_embeddings(embeddings, umap_transform)
+
+
+retrieved_embeddings = results["embeddings"][0]
+original_query_embedding = embedding_function([original_query])
+augmented_query_embedding = embedding_function([joint_query])
+
+projected_original_query_embedding = project_embeddings(
+    original_query_embedding, umap_transform
+)
+projected_augmented_query_embedding = project_embeddings(
+    augmented_query_embedding, umap_transform
+)
+projected_retrieved_embeddings = project_embeddings(
+    retrieved_embeddings, umap_transform
+)
--- advanced_rag_openai/helper_utils.py
+++ advanced_rag_openai/helper_utils.py
@@ -1,3 +1,8 @@
 # Wraps text to a certain width
 def word_wrap(text, width=87):
     return "\n".join([text[i : i + width] for i in range(0, len(text), width)])
+
+# Project embeddings using UMAP
+def project_embeddings(embeddings, umap_transform):
+    projected_embeddings = umap_transform.transform(embeddings)
+    return projected_embeddings
